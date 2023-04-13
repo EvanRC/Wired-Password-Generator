ad721966700f7d7f9ed0730624ca6a3543c47c6c
Evan Carlson <evanrc1@gmail.com>
2023-04-12 17:30:38 -0700

    added error message

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -19,12 +19,37 @@ function generatePassword() {
     return "please enter valid length"
   }
   var upperCaseLetters = confirm ("Would you like to have uppercase letters in your password?")
-  var upperLowerCase = confirm ("Would you like to have Lowercase letters in your password?")
+  var lowerCaseLetters = confirm ("Would you like to have Lowercase letters in your password?")
   var numbers = confirm ("Would you like to have numbers in your password?")
   var symbols = confirm ("Would you like to have symbols in your password?")
-  
-  
-
+  var characters = []
+  if (upperCaseLetters) {
+    characters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  }
+  if (lowerCaseLetters) {
+    characters += "abcdefghijklmnopqrstuvwxyz"
+  }
+  if (numbers) {
+    characters += "0123456789"
+  }
+  if (symbols) {
+    characters += "!@#$%^&*+=?"
+  }  
+  /*
+  var str = "123"
+  var num = 123
+  if(str == num){  // just value comparison
+    console.log("true")
+  }
+  if(str === num){ // value and data type
+    console.log("not reached -- false")
+  }else{
+    console.log("Else")
+  }
+  */
+if(characters.length === 0){
+  return "Please choose min one"
+}
 }
 
 // Add event listener to generate button
