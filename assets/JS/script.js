846e6e59bd7604f7dd41f9bb082c07eabbcc9bdf
Evan Carlson <evanrc1@gmail.com>
2023-04-12 17:16:50 -0700

    added promps

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,8 +1,8 @@
 // Assignment code here
 
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
@@ -10,6 +10,20 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
+}
+
+function generatePassword() {
+  var passwordlength = parseInt(prompt ("enter password length"))
+  if (passwordlength < 8 || passwordlength > 126 || isNaN (passwordlength) ){//or ||  and && not !
+    return "please enter valid length"
+  }
+  var upperCaseLetters = confirm ("Would you like to have uppercase letters in your password?")
+  var upperLowerCase = confirm ("Would you like to have Lowercase letters in your password?")
+  var numbers = confirm ("Would you like to have numbers in your password?")
+  var symbols = confirm ("Would you like to have symbols in your password?")
+  
+  
 
 }
 
